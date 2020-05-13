0502282dc8549b22d83ecb1d160d4c4c0fc1a3c1
Adam Ryan <adamryan1983@gmail.com>
2020-05-13 12:42:24 -0230

    added images to the board
    added images to the game

--- game.js
+++ game.js
@@ -3,6 +3,17 @@
 const board = new Array(7);
 let player = 1;
 
+const boardImg = new Image();
+boardImg.src = "/assets/c4Board.png";
+const player1Img = new Image();
+player1Img.src = "/assets/player1.png";
+const player2Img = new Image();
+player2Img.src = "/assets/player2.png";
+
+//offset for images
+const offsetPlayer1 = [14,10];
+const offsetPlayer2 = [14,10];
+
 for (let i = 0; i < 7; i++) {
     board[i] = new Array(6);
     board[i].fill(0);
@@ -29,18 +40,17 @@ function draw() {
   const canvas = document.getElementById('tutorial');
   if (canvas.getContext) {
       let ctx = canvas.getContext('2d');
+      ctx.drawImage(boardImg,0,0);
       for (let row = 0; row <= 6; row++) {
           for (let col = 0; col < 7; col++) {
               if (board[col][row] == 0) {
-                ctx.strokeRect(col*100, row*100, 100, 100);
+                //ctx.strokeRect(col*100, row*100, 100, 100);
               }
               else if (board[col][row] == 1) {
-                ctx.fillStyle = 'rgb(200, 0, 0)';
-                ctx.fillRect(col*100, row*100, 100, 100);
+                ctx.drawImage(player1Img,col*100 + offsetPlayer1[0], row*100 + offsetPlayer1[1]);
               }
               else if (board[col][row] == 2) {
-                ctx.fillStyle = 'rgb(200, 200, 0)';
-                ctx.fillRect(col*100, row*100, 100, 100);
+                ctx.drawImage(player2Img,col*100 + offsetPlayer2[0], row*100 + offsetPlayer2[1]);
               }
           }
       }
