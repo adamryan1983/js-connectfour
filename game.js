8124afd30780d9615ebc61c0ce68e5055e2794ed
Adam Ryan <adamryan1983@gmail.com>
2020-05-13 22:01:37 -0230

    assets added and more code
    vertical win conditions set

--- game.js
+++ game.js
@@ -1,42 +1,104 @@
 "use strict";
 
+const state = {
+  PLAYING: 'playing',
+  REDWINS: 'redwins',
+  YELLOWWINS: 'yellowwins',
+  DRAW: 'draw',
+}
+
+let gameState = state.PLAYING;
+
 const board = new Array(7);
 let player = 1;
 
+//map the images to their location
 const boardImg = new Image();
 boardImg.src = "/assets/c4Board.png";
+
 const player1Img = new Image();
 player1Img.src = "/assets/player1.png";
+player1Img.classList = "fall";
+
 const player2Img = new Image();
 player2Img.src = "/assets/player2.png";
+player2Img.classList = "fall";
+
+const playAgain = new Image();
+playAgain.src = "/assets/playAgain.png";
+
+const player1Wins = new Image();
+player1Wins.src = "/assets/redWins.png";
+
+const player2Wins = new Image();
+player2Wins.src = "/assets/yellowWins.png";
 
 //offset for images
 const offsetPlayer1 = [14,10];
 const offsetPlayer2 = [14,10];
+
+const checkWin = (col, row, player) => {
+  //check for vertical win
+  let rowCheck = row;
+  let count = 1;
+  while (rowCheck > 0) {
+    rowCheck--;
+    if (board[col][rowCheck] === player) {
+      count++;
+    }
+    else {
+      break;
+    }
+  } 
+   
+  //reset rowCheck
+  rowCheck = row;
+
+  while (rowCheck < 5) {
+    rowCheck++;
+    if (board[col][rowCheck] == player) {
+      count++;
+    }
+    else {
+      break;
+    }
+  }
+
+  if (count >= 4) {
+    gameState = player == 1 ? state.YELLOWWINS : state.REDWINS;
+  }
+}
 
 for (let i = 0; i < 7; i++) {
     board[i] = new Array(6);
     board[i].fill(0);
 }
 
-function placePiece(col) {
+const placePiece = (col) => {
   for (let row = 5; row >=0; row--) {
     if(board[col][row] == 0) {
       if(player == 1) {
         board[col][row] = 1;
+        checkWin(col,row,player);
         player = 2;
       }
       else {
         board[col][row] = 2;
+        checkWin(col,row,player);
         player = 1;
       }
       break;
     }
   }
   draw();
+  console.log(player2Img.className)
 }
 
-function draw() {
+const slide = (col, row) => {
+
+}
+
+const draw = () => {
   const canvas = document.getElementById('tutorial');
   if (canvas.getContext) {
       let ctx = canvas.getContext('2d');
@@ -59,17 +121,36 @@ function draw() {
           //ctx.strokeRect(col*100, row*100, 100, 100);
         }
       }
+
+      if (gameState === state.REDWINS) {
+        ctx.drawImage(player1Wins, 200, 200);
+        ctx.drawImage(playAgain,200,350);
+      }
+      else if (gameState === state.YELLOWWINS) {
+        ctx.drawImage(player2Wins, 200, 200);
+        ctx.drawImage(playAgain,200,350);
+      }
   }
 };
 
-function getMousePosition(canvas, event) { 
+const getMousePosition = (canvas, event) => { 
   let rect = canvas.getBoundingClientRect(); 
   let x = event.clientX - rect.left; 
   let y = event.clientY - rect.top; 
 
-  let col = parseInt(x / 100);
-  placePiece(col);
-  console.log(col);
+  if ( gameState === state.PLAYING ) {
+    let col = parseInt(x / 100);
+    placePiece(col);
+  }
+  else {
+    if (x >= 200 && x <=532 && y >= 350 && y <= 449) {
+      gameState = state.PLAYING;
+      for (let i = 0; i < 7; i++) {
+        board[i].fill(0);
+      }
+      draw();
+    }
+  }
 } 
 
 window.onload = () => {
